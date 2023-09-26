f27cab24d729d6d43ecf0b99143dbce5ce31f8d6
YZK-mini <129926335+YZK-mini@users.noreply.github.com>
2023-09-26 20:14:30 +0800

    socket初始化设置，定义红、黑方信息传输格式（用于传输棋子的行动）

--- Black.py
+++ Black.py
@@ -1,1 +1,14 @@
-import socket
+from socket import *
+import pickle
+
+ip_black_server = ('127.0.0.1', 5000)  # 当黑方发起游戏的ip地址
+ip_black_client = ('127.0.0.1', 8000)  # 当黑方加入游戏需要连接的ip地址
+buf_size = 512  # 传输的尺寸限制
+tag = 0  # 游戏进行状态标识，0表示开始界面，1表示等待界面，2表示游戏界面，3表示结束界面
+
+
+# 用于传送数据的类
+class Mess:
+    def __init__(self):
+        self.tag = 0
+        self.text = ''
--- Red.py
+++ Red.py
@@ -1,1 +1,14 @@
-import socket
+from socket import *
+import pickle
+
+ip_red_server = ('127.0.0.1', 8000)  # 当红方发起游戏的ip地址
+ip_red_client = ('127.0.0.1', 5000)  # 当红方参加游戏需要连接的端口
+buf_size = 512  # 传输的尺寸限制
+tag = 0  # 游戏进行状态标识，0表示开始界面，1表示等待界面，2表示游戏界面，3表示结束界面
+
+
+# 用于传送数据的类
+class Mess:
+    def __init__(self):
+        self.tag = 0
+        self.text = ''
