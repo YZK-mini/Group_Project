2d24092e167e3b6212835e0eaec1cc2026c891ba
YZK-mini <129926335+YZK-mini@users.noreply.github.com>
2023-09-26 21:40:29 +0800

    提交’创建数据传输类‘函数

--- Black.py
+++ Black.py
@@ -7,8 +7,16 @@ buf_size = 512  # 传输的尺寸限制
 tag = 0  # 游戏进行状态标识，0表示开始界面，1表示等待界面，2表示游戏界面，3表示结束界面
 
 
-# 用于传送数据的类
-class Mess:
+class Mess:  # 用于传送数据的类
     def __init__(self):
-        self.tag = 0
-        self.text = ''
+        self.beforeX = 0
+        self.beforeY = 0
+        self.afterX = 0
+        self.afterY = 0
+
+
+def create_Mess(msg, PoX, PoY, CuX, CuY):  # 创建数据传输的类，需要本方的移动信息
+    msg.beforeX = PoX
+    msg.beforeY = PoY
+    msg.afterX = CuX
+    msg.afterY = CuY
