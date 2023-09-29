3ee9476e5861b5ee7a9d62d26711d716f943ba2a
YZK-mini <129926335+YZK-mini@users.noreply.github.com>
2023-09-29 15:26:46 +0800

    事件检测函数，与red，black连接建立的标志修改函数

--- Draw_Related.py
+++ Draw_Related.py
@@ -1,14 +1,13 @@
 import pygame
 import sys
-import time
 
 
 # 初始化函数，供black和red调用
 def init():
     global screen, start_img, wait_img, board_img, end_img, start_button1, start_button2, join_button1, join_button2, \
-        return_button1, return_button2, red_return1, red_return2, red_exit1, red_exit2, black_return1, black_return2, \
-        black_exit1, black_exit2
-
+        retn_button1, retn_button2, red_return1, red_return2, red_exit1, red_exit2, black_return1, black_return2, \
+        black_exit1, black_exit2, tag
+    tag = 0
     # 初始化pygame
     pygame.init()
     # 创建窗口
@@ -22,8 +21,8 @@ def init():
     # 按钮’启动游戏‘左上（128，272）右下（390，349），按钮’加入游戏‘左上（126，389）右下（389，462）
 
     wait_img = pygame.image.load('images/等待界面.png')
-    return_button1 = (165, 313)
-    return_button2 = (340, 379)
+    retn_button1 = (165, 313)
+    retn_button2 = (340, 379)
     # 按钮'返回'左上（165，313）右下（340，379）
 
     board_img = pygame.image.load('images/棋盘.png')
@@ -40,26 +39,41 @@ def init():
     # 黑方胜界面中，按钮’返回开始菜单‘左上（114，289）右下（377，366），按钮'退出'左上(183，419)右下（318，483）
 
 
-# 背景绘制
-def bg_draw(tag):
-    global end_img
+# 背景绘制，tg即tag
+def bg_draw(tg):
+    global start_img, wait_img, board_img, end_img
+
     # 背景绘制
-    if tag == 0:
+    if tg == 0:
+        # tag即tg为0时，开始界面，屏幕画上开始界面图片
         screen.blit(start_img, (0, 0))
-    elif tag == 1:
+    elif tg == 1:
+        # tag即tg为1时，等待界面，屏幕画上等待界面图片
         screen.blit(wait_img, (0, 0))
-    elif tag == 2:
+    elif tg == 2:
+        # tag即tg为2时，游戏界面，屏幕画上游戏界面图片
         screen.blit(board_img, (0, 0))
-    elif tag == 30:
+    elif tg == 30:
+        # tag即tg为30时，红方胜界面，屏幕画上红方胜界面图片
         end_img = pygame.image.load('images/红方胜.png')
         screen.blit(end_img, (0, 0))
-    elif tag == 31:
+    elif tg == 31:
+        # tag即tg为31时，黑方胜界面，屏幕画上黑方胜界面图片
         end_img = pygame.image.load('images/黑方胜.png')
         screen.blit(end_img, (0, 0))
 
 
+# 当black与red建立连接后会调用此函数，将tag置为2，即双方进入游戏状态
+def begin():
+    global tag
+
+    tag = 2
+
+
 # 事件检测函数
-def check_movement(tag):
+def check_movement():
+    global tag
+
     # 事件检测
     for event in pygame.event.get():
         # 退出事件
@@ -67,32 +81,38 @@ def check_movement(tag):
             pygame.quit()
             sys.exit()
         # 鼠标点击事件
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # 获取鼠标点击坐标
             mouse_presses = pygame.mouse.get_pressed()
             if mouse_presses[0]:
-                ttag = mouse_solve(pygame.mouse.get_pos(), tag)
+                # 调用鼠标事件处理函数，传入鼠标坐标
+                mouse_solve(pygame.mouse.get_pos())
+                return tag
+    return tag
 
 
 # 鼠标事件处理函数
-def mouse_solve(mouse_pos, tag):
+def mouse_solve(mouse_pos):
+    global tag
+
     # 若处于开始界面
     if tag == 0:
         # 判断是否点击‘启动游戏’按钮
         if start_button1[0] < mouse_pos[0] < start_button2[0] and start_button1[1] < mouse_pos[1] < start_button2[1]:
-            return 1
+            tag = 1
         # 判断是否点击‘加入游戏’按钮
         if join_button1[0] < mouse_pos[0] < join_button2[0] and join_button1[1] < mouse_pos[1] < join_button2[1]:
-            return 2
+            tag = 2
     # 若处于等待界面
     if tag == 1:
         # 判断是否点击‘返回’按钮
-        if return_button1[0] < mouse_pos[0] < return_button2[0] and return_button1[1] < mouse_pos[1] < return_button2[1]:
-            return 0
+        if retn_button1[0] < mouse_pos[0] < retn_button2[0] and retn_button1[1] < mouse_pos[1] < retn_button2[1]:
+            tag = 0
     # 若处于结束界面
     if tag == 30:
         # 判断是否点击’返回开始界面‘按钮
         if red_return1[0] < mouse_pos[0] < red_return2[0] and red_return1[1] < mouse_pos[1] < red_return2[1]:
-            return 0
+            tag = 0
         # 判断是否点击'退出'按钮
         if red_exit1[0] < mouse_pos[0] < red_exit2[0] and red_exit1[1] < mouse_pos[1] < red_exit2[1]:
             pygame.quit()
@@ -100,7 +120,7 @@ def mouse_solve(mouse_pos, tag):
     if tag == 31:
         # 判断是否点击’返回开始界面‘按钮
         if black_return1[0] < mouse_pos[0] < black_return2[0] and black_return1[1] < mouse_pos[1] < black_return2[1]:
-            return 0
+            tag = 0
         # 判断是否点击'退出'按钮
         if black_exit1[0] < mouse_pos[0] < black_exit2[0] and black_exit1[1] < mouse_pos[1] < black_exit2[1]:
             pygame.quit()
